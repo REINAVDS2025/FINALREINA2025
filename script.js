4c9a7b8eb3f1f811e477b584b93bcdd0722d63ef
REINAVDS2025 <reinavds2025@gmail.com>
2025-05-13 21:46:28 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const form = document.getElementById('votingForm');
+const sheetURL = 'https://script.google.com/macros/s/AKfycbz-I5wJROiVC6yx6Lr98rsRFOKQugH50Nm6O_eSw-Qso3QUJMQDlGikRXIUbZZSHE7_nA/exec';
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Validar si ya votó
+  if (document.cookie.includes('votado=true')) {
+    alert("¡Ya has votado! Gracias por tu participación.");
+    return;
+  }
+
+  const candidata = document.querySelector('input[name="candidata"]:checked');
+  if (!candidata) {
+    alert("Selecciona una candidata antes de votar.");
+    return;
+  }
+
+  fetch(sheetURL, {
+    method: 'POST',
+    mode: 'no-cors',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: `voto=${encodeURIComponent(candidata.value)}`
+  });
+
+  document.cookie = "votado=true; max-age=" + (60 * 60 * 24 * 30); // 30 días
+  alert("¡Gracias por tu voto!");
+  form.reset();
+});
