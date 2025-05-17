69dd4e2900ab17f150e403b74ff9caac3178b5ba
REINAVDS2025 <reinavds2025@gmail.com>
2025-05-17 02:52:10 -0600

    script.js

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
-const form = document.getElementById('votingForm');
-const sheetURL = 'https://script.google.com/macros/s/AKfycbzR8vXTVQdLB1Lj4OwUgZt2ewyM23Jx1nT6W9IGCXTVZglkTET9FJ8H7GeIDBwtESWx/exec';
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
+document.getElementById("votingForm").addEventListener("submit", function(event) {
+  event.preventDefault();
 
   // Validar si ya votó
-  if (document.cookie.includes('votado=true')) {
+  if (document.cookie.includes("votado=true")) {
     alert("¡Gracias por participar! Tu voto ya ha sido registrado. Solo se permite un voto por persona.");
     return;
   }
 
-  const candidata = document.querySelector('input[name="candidata"]:checked');
-  if (!candidata) {
-    alert("Selecciona una candidata antes de votar.");
-    return;
-  }
+  const seleccion = document.querySelector('input[name="candidata"]:checked');
+  if (!seleccion) return alert("Selecciona una candidata.");
 
-  // Enviar el voto
-  fetch(sheetURL, {
-    method: 'POST',
-    mode: 'no-cors',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: voto=${encodeURIComponent(candidata.value)}
+  const voto = seleccion.value;
+
+  const urls = [
+    "https://script.google.com/macros/s/AKfycbzcuA6W3UERKn0cIIsuTPZkZx2iIqL89VOsDfubUUfTxXgkacUphgX67NQH-2Wd-lI8xA/exec",
+    "https://script.google.com/macros/s/AKfycbzpNcPpgx807b8QMW11RWatvjWyTqCCJ00o4x42yyHR3qcxivZfvvNSmoLIw1BDjlh8/exec",
+    "https://script.google.com/macros/s/AKfycbwFWi1p_b5MrKPY15FSXSuz5Dxso1Oyfj0WqX39wEV7I21PtaEU07EKsEZypXyuBn2O/exec",
+    "https://script.google.com/macros/s/AKfycbyzvNn27Dgg5nf4ZpPni10iq3MoIshJUoT_qGFk4zKB8thDmlChBgWeW0ZtnniUt48p/exec",
+    "https://script.google.com/macros/s/AKfycbyrtI9nAkDALmDAxiomfIExhcNgok6BmWkrHiDWKHkQwYVdwOEc1MSNvAWky5mYlJKJ/exec",
+    "https://script.google.com/macros/s/AKfycbyz53OOKxgMndso-gDxH54iT5zTuq88GtENq_Qt3sFd8grnbRr1Lji3ifgz7yxrE2rKdA/exec"
+  ];
+
+  const url = urls[Math.floor(Math.random() * urls.length)];
+
+  fetch(url, {
+    method: "POST",
+    mode: "no-cors",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: `voto=${encodeURIComponent(voto)}`
   });
 
-  // Guardar cookie para evitar otro voto
   document.cookie = "votado=true; max-age=" + (60 * 60 * 24 * 30); // 30 días
-
-  // Redirigir a la página de agradecimiento
   window.location.href = "gracias.html";
 });
