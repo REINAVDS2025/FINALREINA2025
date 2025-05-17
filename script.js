51c89eed7480d24699c6b4e5cccf1ac02ff45a57
REINAVDS2025 <reinavds2025@gmail.com>
2025-05-17 03:02:34 -0600

    script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.getElementById("votingForm").addEventListener("submit", function(event)
   const urls = [
     "https://script.google.com/macros/s/AKfycbzcuA6W3UERKn0cIIsuTPZkZx2iIqL89VOsDfubUUfTxXgkacUphgX67NQH-2Wd-lI8xA/exec",
     "https://script.google.com/macros/s/AKfycbzpNcPpgx807b8QMW11RWatvjWyTqCCJ00o4x42yyHR3qcxivZfvvNSmoLIw1BDjlh8/exec",
-    "https://script.google.com/macros/s/AKfycbwFWi1p_b5MrKPY15FSXSuz5Dxso1Oyfj0WqX39wEV7I21PtaEU07EKsEZypXyuBn2O/exec",
+    "https://script.google.com/macros/s/AKfycbxDUA7eV2TSdO-MAWp4S7YCstoS4LKylkuTUQ_a2z7Dvr3Mp2p3KQzoyz02RkWZmW7I/exec",
     "https://script.google.com/macros/s/AKfycbyzvNn27Dgg5nf4ZpPni10iq3MoIshJUoT_qGFk4zKB8thDmlChBgWeW0ZtnniUt48p/exec",
     "https://script.google.com/macros/s/AKfycbyrtI9nAkDALmDAxiomfIExhcNgok6BmWkrHiDWKHkQwYVdwOEc1MSNvAWky5mYlJKJ/exec",
     "https://script.google.com/macros/s/AKfycbyz53OOKxgMndso-gDxH54iT5zTuq88GtENq_Qt3sFd8grnbRr1Lji3ifgz7yxrE2rKdA/exec"
