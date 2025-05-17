3cea5cf50002e3938fe9d83fec78c191518e8160
REINAVDS2025 <reinavds2025@gmail.com>
2025-05-17 01:10:32 -0600

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 document.getElementById("votingForm").addEventListener("submit", function(event) {
   event.preventDefault();
+
+  // Validar si ya votó
+  if (document.cookie.includes("votado=true")) {
+    alert("¡Gracias por participar! Tu voto ya ha sido registrado. Solo se permite un voto por persona.");
+    return;
+  }
+
   const seleccion = document.querySelector('input[name="candidata"]:checked');
   if (!seleccion) return alert("Selecciona una candidata.");
 
   const voto = seleccion.value;
 
-  // Lista de endpoints
   const urls = [
     "https://script.google.com/macros/s/AKfycbzcuA6W3UERKn0cIIsuTPZkZx2iIqL89VOsDfubUUfTxXgkacUphgX67NQH-2Wd-lI8xA/exec",
     "https://script.google.com/macros/s/AKfycbzpNcPpgx807b8QMW11RWatvjWyTqCCJ00o4x42yyHR3qcxivZfvvNSmoLIw1BDjlh8/exec",
@@ -15,7 +21,6 @@ document.getElementById("votingForm").addEventListener("submit", function(event)
     "https://script.google.com/macros/s/AKfycbyz53OOKxgMndso-gDxH54iT5zTuq88GtENq_Qt3sFd8grnbRr1Lji3ifgz7yxrE2rKdA/exec"
   ];
 
-  // Seleccionar una al azar
   const url = urls[Math.floor(Math.random() * urls.length)];
 
   fetch(url, {
@@ -25,7 +30,6 @@ document.getElementById("votingForm").addEventListener("submit", function(event)
     body: `voto=${encodeURIComponent(voto)}`
   });
 
-  // Guardar cookie y redirigir
   document.cookie = "votado=true; max-age=" + (60 * 60 * 24 * 30); // 30 días
   window.location.href = "gracias.html";
 });
