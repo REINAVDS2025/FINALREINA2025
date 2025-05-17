b0797eb854e8c214cc84b73b578eb0e6f1b8a463
REINAVDS2025 <reinavds2025@gmail.com>
2025-05-17 01:36:13 -0600

    script.js

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ document.getElementById("votingForm").addEventListener("submit", function(event)
   // Validar si ya votó
   if (document.cookie.includes("votado=true")) {
     window.location.href = "gracias.html";
-    alert("¡Gracias por participar! Tu voto ya ha sido registrado. Solo se permite un voto por persona.");
-    return;
+    
   }
 
   const seleccion = document.querySelector('input[name="candidata"]:checked');
