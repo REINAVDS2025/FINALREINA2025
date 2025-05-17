d9ad167e419e3c08b8b984622e6ac2535fc640ad
REINAVDS2025 <reinavds2025@gmail.com>
2025-05-17 00:28:41 -0600

    script.js

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ document.getElementById('votingForm').addEventListener('submit', function (e) {
 
   // URLs de los endpoints
   const urls = [
-    "https://script.google.com/macros/s/AKfycbyN-9IjGJ_B2FgJYgI43IBvAvQu-GYa-B5Hx8LgMfUrAlaVV8AfWuM2LbjB-JsTZAoAqA/exec",
-    "https://script.google.com/macros/s/AKfycbwOVFeXMXteDtxqtfFdgTaW57PyvL2HecVGac5VPsVAc-xG-mqFjv4gaifdrTjIRKS8/exec",
-    "https://script.google.com/macros/s/AKfycbzTkqt73RNbgzZSsDe99GRzULuJktBSFUCWOshBWdhU8Z0lUcan7q-dR--Usm71_Kub/exec",
-    "https://script.google.com/macros/s/AKfycbxkiWDuodOSjobjRnPHpUNcgNZDa29GyQTo1Qw9qANUurrp_0MG4sCNHilKEHZnQxv1/exec",
-    "https://script.google.com/macros/s/AKfycbwGd8O_mOV-TBnaWeJDc_9EIFgCs-TJqQo3necRnXkiECOJcugjwThcsRkjNQ2uNQYG/exec",
-    "https://script.google.com/macros/s/AKfycbw5bhPuuEiif8STDDHgHa76bPhngBS9l2MhH8wS8ebeLoAbMch4OPdBOrFGxvmrsz1CvA/exec"
+    "https://script.google.com/macros/s/AKfycbzcuA6W3UERKn0cIIsuTPZkZx2iIqL89VOsDfubUUfTxXgkacUphgX67NQH-2Wd-lI8xA/exec",
+    "https://script.google.com/macros/s/AKfycbzpNcPpgx807b8QMW11RWatvjWyTqCCJ00o4x42yyHR3qcxivZfvvNSmoLIw1BDjlh8/exec",
+    "https://script.google.com/macros/s/AKfycbwFWi1p_b5MrKPY15FSXSuz5Dxso1Oyfj0WqX39wEV7I21PtaEU07EKsEZypXyuBn2O/exec",
+    "https://script.google.com/macros/s/AKfycbyzvNn27Dgg5nf4ZpPni10iq3MoIshJUoT_qGFk4zKB8thDmlChBgWeW0ZtnniUt48p/exec",
+    "https://script.google.com/macros/s/AKfycbyrtI9nAkDALmDAxiomfIExhcNgok6BmWkrHiDWKHkQwYVdwOEc1MSNvAWky5mYlJKJ/exec",
+    "https://script.google.com/macros/s/AKfycbyz53OOKxgMndso-gDxH54iT5zTuq88GtENq_Qt3sFd8grnbRr1Lji3ifgz7yxrE2rKdA/exec"
   ];
 
   // Selección aleatoria de una URL
